0677191f1ce93683432d4b1f3546e33e3773d145
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-21 13:03:29 +0000

    card model created

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
+from django.apps import apps
 
-# Register your models here.
+
+class CartModelAdmin(admin.ModelAdmin):
+    list_display = ["created_by", "status", "created_at", "updated_at"]
+    search_fields = ("status", "created_at", "updated_at")
+
+
+class CartItemsModelAdmin(admin.ModelAdmin):
+    list_display = ["cart_id", "product_id", "price", "quantity", "created_at"]
+    search_fields = ("cart_id", "product_id", "price")
+
+
+admin.site.register(models.Carts, CartModelAdmin)
+admin.site.register(models.CartItems, CartItemsModelAdmin)

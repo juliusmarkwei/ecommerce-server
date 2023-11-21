0f3728543cc44362cd85aae7594eb57d866e5722
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-21 13:06:22 +0000

    added user url

--- main/urls.py
+++ main/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path("admin/", admin.site.urls),
     path("user/", include("api.urls")),
     # path("product/", include("product.urls")),

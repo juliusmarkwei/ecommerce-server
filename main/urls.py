4e48edd9e47b63cfa858c40aa98dca8d20148764
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-22 17:26:07 +0000

    updated users url

--- main/urls.py
+++ main/urls.py
@@ -20,8 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path("admin/", admin.site.urls),
-    path("user/", include("api.urls")),
-    # path("product/", include("product.urls")),
+    path("api/", include("api.urls", namespace="api")),
     # path("cart/", include("cart.urls")),
     # path("order/", include("order.urls")),
 ]

3299e61fb6fdfe009d1b22fde11d44f918596b93
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-24 14:05:43 +0000

    updated url for users

--- api/urls.py
+++ api/urls.py
@@ -3,14 +3,12 @@ from django.urls import path, include
 
 app_name = "ecommerce_api"
 urlpatterns = [
-    path("users/", views.UsersList.as_view(), name="users"),
-    # path("users/<int:pk>/", views.UsersRetrieve.as_view(), name="user-rud"),
+    path("users/", views.UsersView.as_view(), name="users"),
     path('users/profiles/', views.SocialProfileList.as_view(), name="profiles"),
     path('users/profiles/<int:pk>/', views.SocialProfileRetrieve.as_view(), name="profiles-rud"),
     path("users/credentials/", views.CredentialsList.as_view(), name="credentials"),
     path("users/credentials/<int:pk>/", views.CredentialsRetrieve.as_view(), name="credentials-rud"),
-    path("products/", views.ProductList.as_view(), name="products"),
-    path("products/<int:pk>/", views.ProductRetrieve.as_view(), name="products-retrieve"),
+    path("products/", views.ProductsView.as_view(), name="products"),
     path("products/categories/", views.CategoriesList.as_view(), name="categories"),
     path("products/categories/<int:pk>/", views.CategoriesRetrieve.as_view(), name="categories-rud"),
     path("products/reviews/", views.ReviewList.as_view(), name="review"),

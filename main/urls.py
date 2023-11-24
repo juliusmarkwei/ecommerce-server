479451d46d98f0923139b990f7e75dfb13d9aed8
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-24 14:06:38 +0000

    included sample url

--- main/urls.py
+++ main/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls", namespace="api")),
+    path("sample/", include("api.sampleUrls", namespace="sample"))
     # path("cart/", include("cart.urls")),
     # path("order/", include("order.urls")),
 ]

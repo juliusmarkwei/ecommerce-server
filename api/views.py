602b9747823a6668ae5dbf6cabf9f02339abe70f
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-25 20:56:32 +0000

    modified product and user view

--- api/views.py
+++ api/views.py
@@ -1,14 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
-from user import models as user_models
-from product import models as product_models
-from cart import models as cart_models
-from order import models as order_models
+from user.models import CustomUser, SocialProfile, Credentials
+from product.models import Products, Categories, Reviews
+from cart.models import Carts, CartItems
+from order.models import Orders, OrderLines
 from rest_framework import generics
 from . import serializers
-from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
+from django.utils.datastructures import MultiValueDictKeyError
 
 
 # users views
@@ -16,14 +16,14 @@ class UsersView(APIView):
     permission_classes = [AllowAny]
 
     def get_queryset(self):
-        users = user_models.CustomUser.objects.all()
+        users = CustomUser.objects.all()
         return users
 
     def get(self, request, *args, **kwargs):
         user_id = None
         try:
             user_id = request.query_params["id"]
-            user = user_models.CustomUser.objects.get(id=user_id)
+            user = CustomUser.objects.get(id=user_id)
             serializer = serializers.CustomUserSerializer(user)
         except:
             print(f"User id {user_id} not found")
@@ -34,7 +34,7 @@ class UsersView(APIView):
     def post(self, request, *args, **kwargs):
         user_data = request.data
         try:
-            user = user_models.CustomUser.objects.create_user(
+            user = CustomUser.objects.create_user(
                 email=user_data["email"],
                 username=user_data["username"],
                 first_name=user_data["first_name"],
@@ -48,27 +48,45 @@ class UsersView(APIView):
         except Exception as e:
             return Response("Error rasies during user creation")
 
+    def delete(self, request, *args, **kwargs):
+        if request.query_params.get("username") != None:
+            username = request.query_params.get("username")
+            print(username)
+            try:
+                user = CustomUser.objects.get(username=username)
+                user.delete()
+
+                return Response({"message": "User deleted successfully"})
+            except CustomUser.DoesNotExist:
+                return Response(
+                    {"message": f"User with username '{username}' not found"},
+                    status=status.HTTP_404_NOT_FOUND,
+                )
+        return Response(
+            {"message": "Provide a 'username' parameter to perform deletion"}
+        )
+
 
 class SocialProfileList(generics.ListCreateAPIView):
-    queryset = user_models.SocialProfile.objects.all()
+    queryset = SocialProfile.objects.all()
     serializer_class = serializers.SocialProfileSerializer
     permission_classes = [AllowAny]
 
 
 class SocialProfileRetrieve(generics.RetrieveUpdateDestroyAPIView):
-    queryset = user_models.SocialProfile.objects.all()
+    queryset = SocialProfile.objects.all()
     serializer_class = serializers.SocialProfileSerializer
     permission_classes = [AllowAny]
 
 
 class CredentialsList(generics.ListCreateAPIView):
-    queryset = user_models.Credentials.objects.all()
+    queryset = Credentials.objects.all()
     serializer_class = serializers.CredentialsSerializer
     permission_classes = [AllowAny]
 
 
 class CredentialsRetrieve(generics.RetrieveUpdateDestroyAPIView):
-    queryset = user_models.Credentials.objects.all()
+    queryset = Credentials.objects.all()
     serializer_class = serializers.CredentialsSerializer
     permission_classes = [AllowAny]
 
@@ -77,109 +95,165 @@ class CredentialsRetrieve(generics.RetrieveUpdateDestroyAPIView):
 class ProductsView(APIView):
     permission_classes = [AllowAny]
 
-    def get(self, request, *args, **kwargs):
-        try:
-            product_name = request.query_params["title"]
-            product = product_models.Products.objects.get(title=product_name)
-            serializer = serializers.ProductsSerializer(product)
+    def get(self, request, pk=None, *args, **kwargs):
+        print(request.user)
+        if pk:
+            try:
+                product = Products.objects.get(pk=pk)
+                serializer = serializers.ProductsSerializer(product)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except Products.DoesNotExist:
+                return Response(
+                    {"message": f"Product with id {pk} not found"},
+                    status=status.HTTP_404_NOT_FOUND,
+                )
+        else:
+            try:
+                product_name = request.query_params["title"]
+                product = Products.objects.get(title=product_name)
+                serializer = serializers.ProductsSerializer(product)
 
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except:
-            products = product_models.Products.objects.all()
-            serializer = serializers.ProductsSerializer(products, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except:
+                products = Products.objects.all()
+                serializer = serializers.ProductsSerializer(products, many=True)
 
-            return Response(serializer.data, status=status.HTTP_200_OK)
+                return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
         product_data = request.data
-        # try:
-        product = product_models.Products.objects.create(
-            title=product_data["title"],
-            category=product_data["category"],
-            description=product_data["description"],
-            tags=product_data["tags"],
-            summary=product_data["summary"],
-            price=product_data["price"],
-            discount_type=product_data["discount_type"],
-            discount_value=product_data["discount_value"],
+
+        category_instance = Categories.objects.get(id=product_data["category"])
+        try:
+            product = Products.objects.create(
+                title=product_data["title"],
+                category=category_instance,
+                description=product_data["description"],
+                tags=product_data["tags"],
+                summary=product_data["summary"],
+                price=product_data["price"],
+                discount_type=product_data["discount_type"],
+                discount_value=product_data["discount_value"],
+            )
+            product.save()
+            serializer = serializers.ProductsSerializer(product)
+
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        except:
+            return Response(
+                "Required fields not provided!", status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def delete(self, request, pk=None, *args, **kwargs):
+        print(request.user)
+
+        if pk != None:
+            try:
+                print("Product about to be queried")
+                product = Products.objects.get(pk=pk)
+                print("Product queried successfully")
+            except Products.DoesNotExist:
+                return Response(
+                    {"message": f"Product with id {pk} not found"},
+                    status=status.HTTP_404_NOT_FOUND,
+                )
+
+        elif "title" in request.query_params:
+            product_title = request.query_params.get("title")
+            try:
+                product = Products.objects.get(title=product_title)
+            except Products.DoesNotExist:
+                return Response(
+                    {"message": f"Product with title '{product_title}' not found"},
+                    status=status.HTTP_404_NOT_FOUND,
+                )
+        else:
+            return Response(
+                {"message": "Provide the product 'id' or 'title' to delete an item"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        product.delete()
+        return Response(
+            {"message": "Item has been deleted"}, status=status.HTTP_204_NO_CONTENT
         )
-        product.save()
-        serializer = serializers.ProductsSerializer(product)
-
-        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-        # except:
-        #     return Response("Post Request received",status=status.HTTP_400_BAD_REQUEST)
 
 
-class CategoriesList(generics.ListCreateAPIView):
-    queryset = product_models.Categories.objects.all()
-    serializer_class = serializers.CategoriesSerializer
+class CategoriesList(APIView):
     permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        if request.GET:
+        # Do something if query parameters are present
+            return Response(f"Query parameters found: {request.GET}")
+        else:
+            # Do something else if no query parameters are present
+            return Response("No query parameters")
 
 
 class CategoriesRetrieve(generics.RetrieveUpdateDestroyAPIView):
-    queryset = product_models.Categories.objects.all()
+    queryset = Categories.objects.all()
     serializer_class = serializers.CategoriesSerializer
     permission_classes = [AllowAny]
 
 
 class ReviewList(generics.ListCreateAPIView):
-    queryset = product_models.Reviews.objects.all()
+    queryset = Reviews.objects.all()
     serializer_class = serializers.ReviewsSerializer
     permission_classes = [AllowAny]
 
 
 class ReviewRetrieve(generics.RetrieveUpdateDestroyAPIView):
-    queryset = product_models.Reviews.objects.all()
+    queryset = Reviews.objects.all()
     serializer_class = serializers.ReviewsSerializer
     permission_classes = [AllowAny]
 
 
 # order views
 class OrderList(generics.ListCreateAPIView):
-    queryset = order_models.Orders.objects.all()
+    queryset = Orders.objects.all()
     serializer_class = serializers.OrdersSerializer
     permission_classes = [AllowAny]
 
 
 class OrderRetrieve(generics.RetrieveUpdateDestroyAPIView):
-    queryset = order_models.Orders.objects.all()
+    queryset = Orders.objects.all()
     serializer_class = serializers.OrdersSerializer
     permission_classes = [AllowAny]
 
 
 class OrderLinesList(generics.ListCreateAPIView):
-    queryset = order_models.OrderLines.objects.all()
+    queryset = OrderLines.objects.all()
     serializer_class = serializers.OrderLinesSerializer
     permission_classes = [AllowAny]
 
 
 class OrderLinesRetrieve(generics.RetrieveUpdateDestroyAPIView):
-    queryset = order_models.OrderLines.objects.all()
+    queryset = OrderLines.objects.all()
     serializer_class = serializers.OrderLinesSerializer
     permission_classes = [AllowAny]
 
 
 # cart views
 class CartsList(generics.ListCreateAPIView):
-    queryset = cart_models.Carts.objects.all()
+    queryset = Carts.objects.all()
     serializer_class = serializers.CartsSerializer
     permission_classes = [AllowAny]
 
 
 class CartsRetieve(generics.RetrieveUpdateDestroyAPIView):
-    queryset = cart_models.Carts.objects.all()
+    queryset = Carts.objects.all()
     serializer_class = serializers.CartsSerializer
     permission_classes = [AllowAny]
 
 
 class CartItemsList(generics.ListCreateAPIView):
-    queryset = cart_models.CartItems.objects.all()
+    queryset = CartItems.objects.all()
     serializer_class = serializers.CartItemsSerializer
     permission_classes = [AllowAny]
 
 
 class CartItemsRetrieve(generics.RetrieveUpdateDestroyAPIView):
-    queryset = cart_models.CartItems.objects.all()
+    queryset = CartItems.objects.all()
     serializer_class = serializers.CartItemsSerializer
     permission_classes = [AllowAny]

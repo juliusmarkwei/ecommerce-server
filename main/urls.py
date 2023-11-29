eeb0c2fcaca71cd0785c74c1b13cd2ded6565ea9
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-29 06:28:03 +0000

    included jwt views

--- main/urls.py
+++ main/urls.py
@@ -16,12 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('api-auth/', include('rest_framework.urls')),
     path("admin/", admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("api/", include("api.urls", namespace="api")),
-    path("sample/", include("api.sampleUrls", namespace="sample"))
-    # path("cart/", include("cart.urls")),
-    # path("order/", include("order.urls")),
+    path("sample/", include("api.sampleUrls", namespace="sample")),
+    
 ]

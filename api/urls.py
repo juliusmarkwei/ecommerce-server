af6400fce1d0d655f8d7d22b881c25ad301c2421
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-29 06:26:55 +0000

    added added token & refresh urls

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,8 @@ from django.urls import path, re_path, include
 app_name = "ecommerce_api"
 urlpatterns = [
     path("users/", views.UsersView.as_view(), name="users"),
-    path('users/profiles/', views.SocialProfileList.as_view(), name="profiles"),
-    path('users/profiles/<int:pk>/', views.SocialProfileRetrieve.as_view(), name="profiles-rud"),
-    path("users/credentials/", views.CredentialsList.as_view(), name="credentials"),
-    path("users/credentials/<int:pk>/", views.CredentialsRetrieve.as_view(), name="credentials-rud"),
+    path('users/profiles/', views.SocialProfileView.as_view(), name="profiles"),
+    path("users/credentials/", views.CredentialsView.as_view(), name="credentials"),
     re_path("products/(?:(?P<pk>\d+)/)?$", views.ProductsView.as_view(), name="products"),
     re_path("products/categories/(?:(?P<pk>\d+)/)?$", views.CategoriesViews.as_view(), name="categories"),
     re_path("products/reviews/(?:(?P<pk>\d+)/)?$", views.ReviewView.as_view(), name="review"),

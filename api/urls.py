500c15954e8cd784c085844f6ef3a4d5646b791d
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-29 15:16:09 +0000

    removed endpoint for sample view

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,6 @@ from django.urls import path, re_path, include
 app_name = "ecommerce_api"
 urlpatterns = [
     path("users/", views.UsersView.as_view(), name="users"),
-    path('users/profiles/', views.SocialProfileView.as_view(), name="profiles"),
-    path("users/credentials/", views.CredentialsView.as_view(), name="credentials"),
     re_path("products/(?:(?P<pk>\d+)/)?$", views.ProductsView.as_view(), name="products"),
     re_path("products/categories/(?:(?P<pk>\d+)/)?$", views.CategoriesViews.as_view(), name="categories"),
     re_path("products/reviews/(?:(?P<pk>\d+)/)?$", views.ReviewView.as_view(), name="review"),
@@ -13,4 +11,5 @@ urlpatterns = [
     path("order-lines/", views.OrderLinesView.as_view(), name="order-lines"),
     path("carts/", views.CartsView.as_view(), name="carts-list"),
     path("cart-items/", views.CartItemsView.as_view(), name="cart-items"),
+    path("verify-email", views.VerifyEmail.as_view(), name="verify-email")
 ]

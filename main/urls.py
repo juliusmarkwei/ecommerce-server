731b328c0ff34681576519975b52370f1161efdd
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-30 06:34:50 +0000

    added django allauth endpoints

--- main/urls.py
+++ main/urls.py
@@ -47,4 +47,5 @@ urlpatterns = [
     path("swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("",schema_view.with_ui("swagger", cache_timeout=0),name="schema-swagger-ui",),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path('rest-auth/', include('rest_auth.urls')),
 ]

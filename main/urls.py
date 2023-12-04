9931b8941d46123ca4c3cc0a4bab99a6c16c9fc3
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-04 18:43:14 +0000

    added back drf jwt paths

--- main/urls.py
+++ main/urls.py
@@ -19,6 +19,10 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 schema_view = get_schema_view(
@@ -37,6 +41,11 @@ schema_view = get_schema_view(
 urlpatterns = [
     #Oauth
     path('auth/', include('drf_social_oauth2.urls', namespace='drf')),
+    
+    #drf jwt
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include("api.urls", namespace="api")),

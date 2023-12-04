f6a33cd0089010dc871ea71b78f9e0446175fbc2
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-04 18:33:50 +0000

    added oauth path

--- main/urls.py
+++ main/urls.py
@@ -16,10 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -39,13 +35,12 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    #Oauth
+    path('auth/', include('drf_social_oauth2.urls', namespace='drf')),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include("api.urls", namespace="api")),
     path("swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("",schema_view.with_ui("swagger", cache_timeout=0),name="schema-swagger-ui",),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-    path('rest-auth/', include('rest_auth.urls')),
 ]

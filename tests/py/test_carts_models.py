14aeb75b20883b483c6806947abf191d0c1a6369
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-08 21:36:26 +0000

    added tests for carts models

--- tests/py/test_carts_models.py
+++ tests/py/test_carts_models.py
@@ -1,1 +1,22 @@
 # testing carts CRUD
+
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
+from django.urls import reverse
+
+
+class TestCart(APITestCase):
+    url = "ecommerce-api:carts"
+    client = APIClient()
+    
+    def test_cart_creation(self):
+        pass
+    
+    def test_cart_retrieval(self):
+        pass
+    
+    def test_cart_update(self):
+        pass
+    
+    def test_cart_deletion(self):
+        pass

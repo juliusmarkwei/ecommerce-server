4f74590a3cf269d3050380c3040e96778e193fcc
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-08 21:35:22 +0000

    updated user model

--- src/user/models.py
+++ src/user/models.py
@@ -64,8 +64,8 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     )
     email = models.EmailField(_("email address"), max_length=100, unique=True)
     phone_regex = RegexValidator(
-        regex=r"^\+?1?\d{9,15}$",
-        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.",
+        regex=r"^\+?1?\d{9,12}$",
+        message="Phone number must be entered in the format: '+999999999'. Up to 12 digits allowed.",
     )
     phone = models.CharField(
         validators=[phone_regex], max_length=20, unique=True, blank=True, null=True

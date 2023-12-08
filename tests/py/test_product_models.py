a02ae0f240e2680f30056bbddadf8a4dcd1b0a86
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-08 21:37:06 +0000

    added tests for product models

--- tests/py/test_product_models.py
+++ tests/py/test_product_models.py
@@ -23,4 +23,29 @@ class TestProduct(APITestCase):
         
         self.client.login(username=testuser1, password=testuser1.password)
         
+        category = Categories.objects.create(
+            name="test category",
+            description="test description",
+            tags="test1 test2"
+        )
+        
+        product = Products.objects.create(
+            title="test product",
+            description="test description",
+            price=100,
+            category=category,
+            summary="test summary",
+            tags="test1 test2",
+            discount_type="test discount type",
+            discount_value=0,
+        )
+        
+        data = {
+            ""
+        }
+        
+        response = self.client.post(self.url, product, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        print(response.data)
+        
         

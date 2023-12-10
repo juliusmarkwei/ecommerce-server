37283ee15a872b58386d07b6ec07a861874b9179
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-10 00:56:13 +0000

    added tests for product models

--- tests/py/test_product_models.py
+++ tests/py/test_product_models.py
@@ -13,19 +13,70 @@ from api.serializers import CategoriesSerializer
 
 
 class TestProduct(APITestCase):
-    url = reverse("ecommerce_api:products")
-    client = APIClient()
-    
-    def test_product_creation(self):        
-        category = Categories.objects.create(
+    def setUp(self):
+        self.product_url = reverse("ecommerce_api:products")
+        self.client = APIClient()
+        
+        self.category1 = Categories.objects.create(
             name="test category 1",
             description="test description",
             tags="test1 test2"
         )
+        self.category2 = Categories.objects.create(
+            name="test category 2",
+            description="test description",
+            tags="test1 test2 test3"
+        )
+        self.category3 = Categories.objects.create(
+            name="test category 3",
+            description="test description",
+            tags="test1 test2 test3 test4"
+        )
+        self.category4 = Categories.objects.create(
+            name="test category 4",
+            description="test description",
+            tags="test1 test2 test3 test4 test5"
+        )
         
-        category_data = CategoriesSerializer(category).data
+        self.product1 = Products.objects.create(
+            title="test product 1",
+            description="test description",
+            price=100.0,
+            category=self.category1,
+            summary="test summary",
+            tags="test1 test2",
+            discount_type="None",
+            discount_value=0,
+        )
+        self.product2 = Products.objects.create(
+            title="test product 2",
+            description="test description",
+            price=100.0,
+            category=self.category2,
+            summary="test summary",
+            tags="test1 test2",
+            discount_type="None",
+            discount_value=0,
+        )
+        self.product3 = Products.objects.create(
+            title="test product 3",
+            description="test description",
+            price=100.0,
+            category=self.category3,
+            summary="test summary",
+            tags="test1 test2",
+            discount_type="None",
+            discount_value=0,
+        )
         
-        data = {
+        self.category_data1 = {
+            "name": "test category 1",
+            "description": "test description",
+            "tags": "test1 test2"
+        }
+        
+        category_data = CategoriesSerializer(self.category1).data
+        self.product_data1 = {
             "title": "test product",
             "description": "test description",
             "price": 100.0,
@@ -35,9 +86,9 @@ class TestProduct(APITestCase):
             "discount_type": "None",
             "discount_value": 0,
         }
-        
-        response = self.client.post(self.url, json.dumps(data), format="json")
-        print(response.data)
+    
+    def test_product_creation(self):        
+        response = self.client.post(self.product_url, json.dumps(self.product_data1), format="json")
         try:
             self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         except AssertionError as e:
@@ -46,36 +97,8 @@ class TestProduct(APITestCase):
             print(f"Exception: {e}")
         
     def test_get_all_product(self):
-        category1 = Categories.objects.create(
-            name="test category 2",
-            description="test description",
-            tags="test1 test2"
-        )
-                
-        product1 = Products.objects.create(
-            title="test product 1",
-            description="test description",
-            price=100.0,
-            category=category1,
-            summary="test summary",
-            tags="test1 test2",
-            discount_type="None",
-            discount_value=0,
-        )
-        
-        product2 = Products.objects.create(
-            title="test product 2",
-            description="test description",
-            price=100.0,
-            category=category1,
-            summary="test summary",
-            tags="test1 test2",
-            discount_type="None",
-            discount_value=0,
-        )
-        
         try:
-            response = self.client.get(self.url, format=None)
+            response = self.client.get(self.product_url, format=None)
             self.assertEqual(response.status_code, status.HTTP_200_OK)
         except AssertionError as e:
             print(f"AssertionError: {e}")
@@ -84,109 +107,47 @@ class TestProduct(APITestCase):
             
             
     def test_get_a_product(self):
-        category1 = Categories.objects.create(
-            name="test category 3",
-            description="test description",
-            tags="test1 test2"
-        )
-        
-        product1 = Products.objects.create(
-            title="test product 1",
-            description="test description",
-            price=43.0,
-            category=category1,
-            summary="test summary",
-            tags="test1 test2 test3",
-            discount_type="None",
-            discount_value=0,
-        )
-        
-        response = self.client.get(self.url + "?" + urlencode({"name": "test product 1"}))
+        response = self.client.get(self.product_url + "?" + urlencode({"name": "test product 1"}))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
-        response = self.client.get(self.url + "?" + urlencode({"id": 1}))
+        response = self.client.get(self.product_url + "?" + urlencode({"id": 1}))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
         
     def test_delete_a_product(self):
-        category1 = Categories.objects.create(
-            name="test category 3",
-            description="test description",
-            tags="test1 test2"
-        )
-        
-        product1 = Products.objects.create(
-            title="test product 4",
-            description="test description",
-            price=43.0,
-            category=category1,
-            summary="test summary",
-            tags="test1 test2 test3",
-            discount_type="None",
-            discount_value=0,
-        )
-        
-        response = self.client.delete(self.url + "?" + urlencode({"title": "test product 4"}))
+        response = self.client.delete(self.product_url + "?" + urlencode({"title": "test product 1"}))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(response.data["message"], "Item has been deleted")
         
         
     def test_category_creation(self):
-        data = {
-            "name": "test category 1",
-            "description": "test description",
-            "tags": "test1 test2"
-        }
-        
-        url = reverse("ecommerce_api:categories")
-        response = self.client.post(url, data, format="json")
+        category_list_url = reverse("ecommerce_api:category-list")
+        response = self.client.post(category_list_url, self.category_data1, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         
         
     def test_get_all_categories(self):
-        category1 = Categories.objects.create(
-            name="test category 1",
-            description="test description",
-            tags="test1 test2 test3 test4"
-        )
-        
-        category2 = Categories.objects.create(
-            name="test category 2",
-            description="test description",
-            tags="test1 test2 test3 test4"
-        )
-        
-        url = reverse("ecommerce_api:categories")
-        response = self.client.get(url, format=None)
+        category_list_url = reverse("ecommerce_api:category-list")
+        response = self.client.get(category_list_url, format=None)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
         
     def test_get_a_category(self):
-        category1 = Categories.objects.create(
-            name="test category 1",
-            description="test description",
-            tags="test1 test2 test3 test4"
-        )
-        
-        url = reverse("ecommerce_api:categories")
-        response = self.client.get(url + "?" + urlencode({"name": "test category 1"}))
+        category_list_url = reverse("ecommerce_api:category-list")
+        response = self.client.get(category_list_url + "?" + urlencode({"name": "test category 1"}))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
-        response = self.client.get(url, kwargs={"pk": 1})
+        category_detail_url = reverse("ecommerce_api:category-detail", kwargs={"pk": 3})
+        response = self.client.get(category_detail_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     
     def test_delete_a_category(self):
-        category1 = Categories.objects.create(
-            name="test category 1",
-            description="test description",
-            tags="test1 test2 test3 test4 test5"
-        )
-        
-        print(category1.id)
-        url = reverse("ecommerce_api:categories")
-        response = self.client.delete(url + "?" + urlencode({"name": "test category 1"}))
+        print(f"Category 4 id : {self.category4.id}")
+        category_list_url = reverse("ecommerce_api:category-list")
+        response = self.client.delete(category_list_url + "?" + urlencode({"name": "test category 3"}))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         
-        response = self.client.delete(url, kwargs={"pk": 1})
+        category_detail_url = reverse("ecommerce_api:category-detail", kwargs={"pk": 4})
+        response = self.client.delete(category_detail_url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)

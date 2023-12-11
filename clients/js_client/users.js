61034c322c21be9aaf6c1812b44e57899ca8e06d
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-11 15:42:43 +0000

    request for post, get and delete

--- clients/js_client/users.js
+++ clients/js_client/users.js
@@ -1,1 +1,79 @@
 //testing users CRUD
+
+import axios from 'axios'
+
+const baseURL = 'http://localhost:8000/api'
+const testuser1 = {
+    "username": "testuser1",
+    "email": "ulooza@wice.pm",
+    "first_name": "Cole",
+    "last_name": "Harris",
+    "password": "12345678",
+    "phone": "test123",
+    // "is_active": true,
+}
+
+const testuser2 = {
+    "username": "testuser2",
+    "email": "haldot@sise.pm",
+    "first_name": "Hal",
+    "last_name": "Dot",
+    "password": "test123",
+    "phone": "(259) 873-9769"
+}
+
+const testuser3 = {
+    "username": "testuser3",
+    "email": "esrodnu@piwoz.ml",
+    "first_name": "Clayton",
+    "last_name": "Abbott",
+    "password": "test123",
+    "phone": "(739) 740-1265"
+}
+
+//  adding user to database
+const user1 = axios.post(baseURL + "/users/", testuser1).then(
+    (response) => {console.log(response.status)}
+).catch((error) => {
+    console.error(error)
+    }
+).finally(() => console.log("POST request completed!"))
+
+
+const user2 = axios.post(baseURL + "/users/", testuser2).then(
+    (response) => {console.log(response.status)}
+).catch((error) => {
+    console.error(error)
+    }
+).finally(() => console.log("POST request completed!"))
+
+
+const user3 = axios.post(baseURL + "/users/", testuser3).then(
+    (response) => {console.log(response.status)}
+).catch((error) => {
+    console.error(error)
+    }
+).finally(() => console.log("POST request completed!"))
+
+
+// delete a user
+axios.delete(baseURL + "/users/" + "?" + "username=Ara.Senger").then(
+    (response) => {
+        console.log(response.status)
+    }
+).catch(
+    (error) => {
+        console.error(error)
+    }
+).finally(() => console.log("DELETE request completed!"))
+
+// get a user
+axios.get(baseURL + "/users/").then(
+    (response) => {
+        console.log({"data": response.data.slice(0, 2)})
+    }
+).catch(
+    (error) => {
+        console.error(error)
+    }
+).finally(() => console.log("GET request completed!"))

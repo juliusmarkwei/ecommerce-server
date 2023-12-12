8da9f6144f6ea81d157e687dd4a91382a1e138b4
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-12 11:48:59 +0000

    field is_active auto true

--- src/user/models.py
+++ src/user/models.py
@@ -89,7 +89,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     bio = models.TextField(max_length=200, blank=True)
     company = models.CharField(max_length=200, blank=True)
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     objects = UserManager()
 

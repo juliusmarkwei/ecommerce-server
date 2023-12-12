aa653f1c552f40d730e7ce0f4e0773775acf57ba
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-12 10:16:22 +0000

    completed product requests

--- clients/js_client/product.js
+++ clients/js_client/product.js
@@ -17,24 +17,11 @@ const handleUserPostRequest = async () => {
         console.log(() => response.status)
         console.log(() => response.data)
     }catch (error){
-        console.error();
+        console.error(error);
     }
 }
 handleUserPostRequest();
 
-// const login = async () => {
-//     try{
-//         const token = btoa(testuser.username + ":" + testuser.password);
-//         const config = {
-//             headers: {"Authorization": "Basic " + token}
-//         }
-//         const response = await axios.post(baseURL + "api-auth/login/", config)
-//         console.log("Login Succesfully!", response.data)
-//     }catch(error){
-//         console.error(error);
-//     }
-// }
-// login();
 
 const handleCategoryPostGetrequest = async () => {
     try{
@@ -44,42 +31,66 @@ const handleCategoryPostGetrequest = async () => {
             tags: "farming tag"
         }
         await axios.post(baseURL + "api/products/categories/", categoryData);
-        console.log(() => {
+        console.log({
             message: "Categry POST completed successfully!"
         })
 
         const response = await axios.get(baseURL + "api/products/categories/" + "?" + `name=${categoryData.name}`);
         return response.data
     }catch(error){
-        console.error();
+        console.error(error);
     }
 }
+handleCategoryPostGetrequest();
 
-const product_data = {
-    "title": "Test Product",
-    "description": "This is a test product",
+
+const product_data1 = {
+    "title": "Test Product 1",
+    "description": "This is a test product 1",
     "price": 3000.00,
-    "summary": "This is a summary",
-    "category": handleCategoryPostGetrequest(),
+    "summary": "This is a summary 1",
+    "category": 7,
     "tags": "test, product, test product",
-    "discount_type": "None",
+    "discount_type": None,
+    "discount_value": 0.0
+}
+
+const product_data2 = {
+    "title": "Test Product 2",
+    "description": "This is a test product",
+    "price": 1400.00,
+    "summary": "This is a summary",
+    "category": 3,
+    "tags": "test, product, test product",
+    "discount_type": None,
     "discount_value": 0.0
 }
 
 
 axios.defaults.withCredentials = true;
-const handleProductPostRequest = async () => {
+const handleProductPostRequest = async (product_data) => {
     try{
         const response = await axios.post(baseURL + "api/products/", product_data);
-        console.log(() => {
-            status_code: response.status;
+        console.log({
+            status_code: response.status,
             data: response.data
         });
     }catch(error){
         console.error(error);
     }
 }
-handleProductPostRequest();
+handleProductPostRequest(product_data1);
+handleProductPostRequest(product_data2);
 
 
-const handleDeleteProductRequest = 
+const handleDeleteProductRequest = async () => {
+    try{
+        await axios.delete(baseURL + "api/products" + "?" + "title=" + product_data1.title);
+        console.log({
+            message: "Product deleted successfully!"
+        })
+    }catch(error){
+        console.error(error);
+    }
+}
+handleDeleteProductRequest();

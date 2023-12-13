935b421a18e8776dc7c5840853bea39138e61890
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-13 13:39:59 +0000

    vercel configs

--- main/settings/settings.py
+++ main/settings/settings.py
@@ -140,6 +140,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = os.path.join(BASE_DIR, '/assets/static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', '/assets/static')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- main/urls.py
+++ main/urls.py
@@ -23,6 +23,9 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 schema_view = get_schema_view(
@@ -53,3 +56,7 @@ urlpatterns = [
     path("",schema_view.with_ui("swagger", cache_timeout=0),name="schema-swagger-ui",),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

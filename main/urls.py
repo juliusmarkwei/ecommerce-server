dff4c092d6872b6b45af7e9e82dd7efc0f82adf1
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-18 18:39:26 +0000

    description and permission for schema view

--- main/urls.py
+++ main/urls.py
@@ -1,35 +1,32 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+from rest_framework.permissions import AllowAny
 
 
-
-schema_view = get_schema_view(
+schema_view = swagger_get_schema_view(
     openapi.Info(
-        title="ECOMMERCE API",
+        title="Ecommerce API",
         default_version="v1",
-        description="Test description",
+        description="A fully functional Ecommerce - Django Rest API project build and tested with restframework and deployed to vercel",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="juliusmarkwei2000@gmail.com"),
-        license=openapi.License(name="MIT License"),
+        license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    permission_classes=[AllowAny],
+    
 )
 
-urlpatterns = [
-    #Oauth
-    path('auth/', include('drf_social_oauth2.urls', namespace='drf')),
-    
-    #default drf suthentincation
+urlpatterns = [    
+    #default drf authentincation
     path("api-auth/", include("rest_framework.urls")),
     
     #drf jwt
@@ -38,9 +35,7 @@ urlpatterns = [
 
     path("admin/", admin.site.urls),
     path("api/", include("api.urls", namespace="api")),
-    path("swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
-    path("",schema_view.with_ui("swagger", cache_timeout=0),name="schema-swagger-ui",),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("", schema_view.with_ui("swagger", cache_timeout=0), name="swagger-schema")
 ]
 
 

c90d4f4fc15b30bc8f1afe53abdd109bf391b0fe
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-18 13:05:16 +0000

    moved api-auth/ up

--- main/urls.py
+++ main/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for main project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import permissions
@@ -45,12 +29,14 @@ urlpatterns = [
     #Oauth
     path('auth/', include('drf_social_oauth2.urls', namespace='drf')),
     
+    #default drf suthentincation
+    path("api-auth/", include("rest_framework.urls")),
+    
     #drf jwt
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
+
     path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls")),
     path("api/", include("api.urls", namespace="api")),
     path("swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("",schema_view.with_ui("swagger", cache_timeout=0),name="schema-swagger-ui",),

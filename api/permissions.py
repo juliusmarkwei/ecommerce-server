631e9297d5a1a2f377a09df072e4da637b0f4cf9
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-18 18:40:42 +0000

    updated permissions

--- api/permissions.py
+++ api/permissions.py
@@ -1,27 +1,12 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework import permissions
 
-# Custom Permission Classes
-class AllowPostRequests(BasePermission):
-    """
-    Permission to allow anyone to make POST requests.
-    """
+class IsPostOrIsAuthenticated(permissions.BasePermission):        
+
     def has_permission(self, request, view):
-        return request.method == "POST"
-
-class IsOwnerOrAdmin(BasePermission):
-    """
-    Allows access only to admin users or the owner of the account for DELETE requests.
-    """
-    def has_object_permission(self, request, view, obj):
-        if request.method == 'DELETE':
-            return obj.username == request.user or request.user.is_superuser
-        return False
-
-class IsAdminUserOrReadOnly(BasePermission):
-    """
-    Allows access only to admin users for non-safe methods.
-    """
-    def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
+        # allow all POST requests
+        if request.method == 'POST':
             return True
-        return request.user and request.user.is_superuser
+
+        # Otherwise, only allow authenticated requests
+        # Post Django 1.10, 'is_authenticated' is a read-only attribute
+        return request.user and request.user.is_authenticated

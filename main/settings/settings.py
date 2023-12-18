476bd87576af448b5953d727e233d9f4940ac97e
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-18 13:06:28 +0000

    changed permission class method

--- main/settings/settings.py
+++ main/settings/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication', # jwt authentication 
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.AllowAny",
+        "rest_framework.permissions.IsAuthenticated",
     ],
 }
 

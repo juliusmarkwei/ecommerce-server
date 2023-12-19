e0f3f3e67cdbfa90d20702fc680bd6196094ab26
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-19 23:19:26 +0000

    added required fields

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,8 @@ from src.user import models as user_models
 from src.product import models as product_models
 from src.cart import models as cart_models
 from src.order import models as order_models
+from collections import OrderedDict
+from rest_framework import serializers
 
 
 # user serializer
@@ -22,6 +24,19 @@ class CustomUserSerializer(ModelSerializer):
             "bio",
             "company",
         ]
+        
+    def get_fields(self):
+        new_fields = OrderedDict()
+        
+        for name, field in super().get_fields().items():
+            if name != 'id':
+                field.required = False
+                new_fields[name] = field
+            else:
+                new_fields[name] = field
+        return new_fields
+    
+        id = serializers.IntegerField()
 
 
 # product serializer
@@ -41,6 +56,19 @@ class ProductsSerializer(ModelSerializer):
             "created_at",
             "updated_at",
         ]
+        
+    def get_fields(self):
+        new_fields = OrderedDict()
+        
+        for name, field in super().get_fields().items():
+            if name != 'id':
+                field.required = False
+                new_fields[name] = field
+            else:
+                new_fields[name] = field
+        return new_fields
+    
+        id = serializers.IntegerField()
 
 
 class CategoriesSerializer(ModelSerializer):
@@ -56,12 +84,38 @@ class CategoriesSerializer(ModelSerializer):
             "created_at",
             "updated_at",
         ]
+        
+    def get_fields(self):
+        new_fields = OrderedDict()
+        
+        for name, field in super().get_fields().items():
+            if name != 'id':
+                field.required = False
+                new_fields[name] = field
+            else:
+                new_fields[name] = field
+        return new_fields
+    
+        id = serializers.IntegerField()
 
 
 class ReviewsSerializer(ModelSerializer):
     class Meta:
         model = product_models.Reviews
         fields = ["id", "user_id", "product", "rating", "comments", "created_at"]
+
+    def get_fields(self):
+        new_fields = OrderedDict()
+        
+        for name, field in super().get_fields().items():
+            if name != 'id':
+                field.required = False
+                new_fields[name] = field
+            else:
+                new_fields[name] = field
+        return new_fields
+    
+        id = serializers.IntegerField()
 
 
 # Oder serializer
@@ -70,20 +124,72 @@ class OrdersSerializer(ModelSerializer):
         model = order_models.Orders
         fields = ["id", "user", "created_at"]
 
+    def get_fields(self):
+        new_fields = OrderedDict()
+        
+        for name, field in super().get_fields().items():
+            if name != 'id':
+                field.required = False
+                new_fields[name] = field
+            else:
+                new_fields[name] = field
+        return new_fields
+    
+        id = serializers.IntegerField()
+        
 
 class OrderLinesSerializer(ModelSerializer):
     class Meta:
         model = order_models.OrderLines
         fields = ["id", "order", "product", "price", "quantity"]
 
+    def get_fields(self):
+        new_fields = OrderedDict()
+        
+        for name, field in super().get_fields().items():
+            if name != 'id':
+                field.required = False
+                new_fields[name] = field
+            else:
+                new_fields[name] = field
+        return new_fields
+    
+        id = serializers.IntegerField()
+        
 
 class CartsSerializer(ModelSerializer):
     class Meta:
         model = cart_models.Carts
         fields = ["id", "created_by", "status", "created_at", "updated_at"]
 
+    def get_fields(self):
+        new_fields = OrderedDict()
+        
+        for name, field in super().get_fields().items():
+            if name != 'id':
+                field.required = False
+                new_fields[name] = field
+            else:
+                new_fields[name] = field
+        return new_fields
+    
+        id = serializers.IntegerField()
+        
 
 class CartItemsSerializer(ModelSerializer):
     class Meta:
         model = cart_models.CartItems
-        fields = ["id", "product_id", "cart_id", "quantity", "created_at"]
+        fields = ["id", "product_id", "price", "cart_id", "quantity", "created_at"]
+
+    def get_fields(self):
+        new_fields = OrderedDict()
+        
+        for name, field in super().get_fields().items():
+            if name != 'id':
+                field.required = False
+                new_fields[name] = field
+            else:
+                new_fields[name] = field
+        return new_fields
+    
+        id = serializers.IntegerField()

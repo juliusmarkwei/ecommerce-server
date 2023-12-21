f7cedb24f51217ef0aeb05619a011f5868ad3481
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-12-21 13:51:44 +0000

    added different views for users models

--- api/urls.py
+++ api/urls.py
@@ -4,11 +4,13 @@ from django.urls import path, re_path, include
 app_name = "ecommerce_api"
 
 urlpatterns = [
-    path("users/", views.UsersView.as_view(), name="users-view"),
-    re_path("products/(?:(?P<pk>\d+)/)?$", views.ProductsView.as_view(), name="products"),
-    path("products/categories/<int:pk>/", views.CategoryDetailView.as_view(), name="category-detail"),
-    path("products/categories/", views.CategoryListViews.as_view(), name="category-list"),
-    re_path("products/reviews/(?:(?P<pk>\d+)/)?$", views.ReviewView.as_view(), name="review"),
+    path("users/<int:pk>/", views.UsersDetailView.as_view(), name="users-view"),
+    path("users/", views.UsersListView.as_view(), name="users-list"),
+    path("products/<int:pk>/", views.ProductsListView.as_view(), name="products-list"),
+    path("products/", views.ProductsDetailView.as_view(), name="products-detail"),
+    path("categories/<int:pk>/", views.CategoryDetailView.as_view(), name="category-detail"),
+    path("categories/", views.CategoryListViews.as_view(), name="category-list"),
+    re_path("reviews/(?:(?P<pk>\d+)/)?$", views.ReviewView.as_view(), name="review"),
     path("orders/<int:pk>/", views.OrderDetailView.as_view(), name="order-detail"),
     path("orders/", views.OrdersListView.as_view(), name="order-list"),
     path("order-lines/", views.OrderLinesView.as_view(), name="order-lines"),
